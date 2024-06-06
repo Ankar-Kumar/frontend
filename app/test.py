7cae5efed14434b594532e03a424bf1d27b13614
Ankar Kumar Saha <ankarkumarankar572@gmial.com>
2024-06-06 08:35:58 +0600

    all the works done just need to change the logo

--- app/test.py
+++ app/test.py
@@ -8,13 +8,14 @@ from utils import load_data, num_to_char
 from videoToframe import collect_and_pad_single_video  # Adjust import based on your directory structure
 
 # Set the layout to the Streamlit app as wide
-st.set_page_config(layout='wide')
-
+# st.set_page_config(layout='wide')
+st.set_page_config(page_title="LipReading App", page_icon="🧊",layout="wide", initial_sidebar_state = "auto")
+# st.set_page_config(layout='wide')
 # Setup the sidebar
 with st.sidebar: 
     st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
-    st.title('SpeachReading')
-    st.info('This application is originally developed from the deep learning model.')
+    st.title('Lip-Reading')
+    st.info('This application is originally developed by the deep learning model.')
 
 st.title('LipReading App')
 # Generating a list of options or videos
@@ -28,7 +29,7 @@ col1, col2 = st.columns(2)
 if options:
     # Rendering the video
     with col1:
-        st.info('The video below displays the converted video in mp4 format')
+        st.info('Video which is inputted for the testing')
         file_path = os.path.join(clips_dir, selected_video)
         video = open(file_path, 'rb')
         video_bytes = video.read()
@@ -41,10 +42,8 @@ if options:
         st.info('Actual sentence which is spoken')
         st.text(tf.strings.reduce_join(num_to_char(annotations)).numpy().decode('utf-8'))
         
+        st.info('Predicted sentences by our model')  
         
-        
-        
-        st.info('This is the output of the deep learning model as tokens')
         model = load_model()
         
         video_padded, annotations_padded = collect_and_pad_single_video(video, annotations)
@@ -54,6 +53,6 @@ if options:
         yhat = model.predict(video_padded)
         decoder = tf.keras.backend.ctc_decode(yhat, [115], greedy=True)[0][0].numpy()
         
-        st.info('Predicted sentences by our model')        
+              
         converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
         st.text(converted_prediction)
